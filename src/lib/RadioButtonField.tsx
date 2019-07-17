27c888f23eac4be193d63e206dfb10c66e34c400
Romina Manzano <romina.manzano@ecloudsolutions.com>
2019-07-17 10:54:23 -0300

    FIX: RadioButtonField improvements

--- src/lib/RadioButtonField.tsx
+++ src/lib/RadioButtonField.tsx
@@ -2,35 +2,57 @@ import React from 'react';
 import { Field, FieldProps } from 'formik';
 
 export interface RadioButtonProps {
+  className?: string;
+  classNameLabel?: string;
+  disabled?: boolean;
   id: string;
-  label: string;
+  inputStyle?: object;
+  label?: string | null;
+  labelStyle?: object;
   name: string;
 }
 
 export type RenderRadioButton = (props: FieldProps) => React.ReactNode;
 
 const RadioButtonField: React.FC<RadioButtonProps> = ({
-  name,
-  label,
+  className,
+  classNameLabel,
+  disabled,
   id,
+  inputStyle,
+  label,
+  labelStyle,
+  name,
 }: RadioButtonProps) => {
   const renderRadioButton: RenderRadioButton = ({ field: {
     onChange,
     value,
   } }: FieldProps) => {
+    const displayLabel: React.ReactNode = label ?
+      (
+        <label
+          className={classNameLabel}
+          htmlFor={id}
+          style={labelStyle}
+        >
+          {label}
+        </label>
+      ) : null;
+
     return (
       <React.Fragment>
         <input
+          className={className}
+          checked={id === value}
+          disabled={disabled}
           id={id}
           name={name}
           onChange={onChange}
+          style={inputStyle}
           type="radio"
-          checked={id === value}
           value={id}
         />
-        <label htmlFor={id}>
-          {label}
-        </label>
+        {displayLabel}
       </React.Fragment>
     );
   };
@@ -41,6 +63,15 @@ const RadioButtonField: React.FC<RadioButtonProps> = ({
       render={renderRadioButton}
     />
   );
+};
+
+RadioButtonField.defaultProps = {
+  className: '',
+  classNameLabel: '',
+  disabled: false,
+  inputStyle: {},
+  label: null,
+  labelStyle: {},
 };
 
 export default RadioButtonField;
